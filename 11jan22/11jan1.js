bc8ec9683300d7b55f47eba4f6576d3efbb5a822
pradnyadukre <94894962+pradnyadukre@users.noreply.github.com>
2022-01-16 23:41:26 +0530

    Add files via upload

---
+++ 11jan22/11jan1.js
@@ -0,0 +1,10 @@
+var num= prompt("Enter the number");
+
+num = parseInt(num);
+
+for(i = 1; i<= num ; i++){
+       for(j =0; j < i; j++){
+      document.write(" * ");
+    }
+    document.write("</br>");
+} 
